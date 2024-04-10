c22aad14a7bea2f925dfdcddab7c4688501091e1
RomanHlodann <roman.hlodann@gmail.com>
2024-04-10 12:26:40 +0300

    add models Task, Tag and make migrations

--- planner/models.py
+++ planner/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    content = models.CharField(max_length=100)
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_completed = models.BooleanField()
+    tags = models.ManyToManyField("Tag", related_name="tasks")
+
+    def __str__(self) -> str:
+        return f"{self.content}. Is completed: {self.is_completed}."
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
