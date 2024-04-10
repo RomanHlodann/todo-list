b1e9f6924973ef53c354294ddb2f64d519a2e970
RomanHlodann <roman.hlodann@gmail.com>
2024-04-10 14:07:19 +0300

    add urls for tag views

--- planner/urls.py
+++ planner/urls.py
@@ -4,6 +4,10 @@ from planner.views import (
     TaskListView,
     TaskCreateView,
     TaskUpdateView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
 )
 
 
@@ -11,6 +15,10 @@ urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
     path("task/create/", TaskCreateView.as_view(), name="task-create"),
     path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tag/", TagListView.as_view(), name="tag-list"),
+    path("tag/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tag/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tag/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "planner"
