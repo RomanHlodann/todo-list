194d109b48cc667fd57c56e2088321a45ff1c1e7
RomanHlodann <roman.hlodann@gmail.com>
2024-04-10 13:43:21 +0300

    add url to create task

--- planner/urls.py
+++ planner/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from planner.views import TaskListView
+from planner.views import (
+    TaskListView,
+    TaskCreateView,
+)
 
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
+    path("task/create/", TaskCreateView.as_view(), name="task-create")
 ]
 
 app_name = "planner"
