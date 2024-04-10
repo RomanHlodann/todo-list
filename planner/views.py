dd1c9d94b3bd1bc52ba7046eb9a050f353e61b54
RomanHlodann <roman.hlodann@gmail.com>
2024-04-10 13:45:16 +0300

    add view for creating task

--- planner/views.py
+++ planner/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
+from planner.forms import TaskCreationForm
 from planner.models import Task, Tag
 
 
@@ -10,3 +12,10 @@ class TaskListView(generic.ListView):
 
     class Meta:
         ordering = ["is_completed", "-datetime"]
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    success_url = reverse_lazy("planner:index")
+    form_class = TaskCreationForm
+
