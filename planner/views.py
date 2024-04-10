80bb7131312be05e0830ab514e24dbbc11133aa9
RomanHlodann <roman.hlodann@gmail.com>
2024-04-10 14:35:23 +0300

    fix model in TaskUpdateView and ordering in TaskListView

--- planner/views.py
+++ planner/views.py
@@ -9,9 +9,7 @@ from planner.models import Task, Tag
 class TaskListView(generic.ListView):
     model = Task
     queryset = Task.objects.prefetch_related("tags")
-
-    class Meta:
-        ordering = ["is_completed", "-datetime"]
+    ordering = ["is_completed", "-datetime"]
 
 
 class TaskCreateView(generic.CreateView):
@@ -21,7 +19,7 @@ class TaskCreateView(generic.CreateView):
 
 
 class TaskUpdateView(generic.UpdateView):
-    model = Tag
+    model = Task
     success_url = reverse_lazy("planner:index")
     form_class = TaskCreationForm
 
