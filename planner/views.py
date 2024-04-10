a8f338133bf3fe85cf03915f17d15f297f0912f1
RomanHlodann <roman.hlodann@gmail.com>
2024-04-10 14:11:13 +0300

    add view to delete task

--- planner/views.py
+++ planner/views.py
@@ -26,6 +26,11 @@ class TaskUpdateView(generic.UpdateView):
     form_class = TaskCreationForm
 
 
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("planner:index")
+
+
 class TagListView(generic.ListView):
     model = Tag
 
