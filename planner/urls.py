711e27b294d010e24b5e9c84cabbf7905e902c18
RomanHlodann <roman.hlodann@gmail.com>
2024-04-10 14:45:26 +0300

    add url for task toggling

--- planner/urls.py
+++ planner/urls.py
@@ -9,6 +9,7 @@ from planner.views import (
     TagCreateView,
     TagUpdateView,
     TagDeleteView,
+    toggle_task_complete,
 )
 
 
@@ -17,6 +18,7 @@ urlpatterns = [
     path("task/create/", TaskCreateView.as_view(), name="task-create"),
     path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("task/<int:pk>/toggle/", toggle_task_complete, name="task-toggle"),
     path("tag/", TagListView.as_view(), name="tag-list"),
     path("tag/create/", TagCreateView.as_view(), name="tag-create"),
     path("tag/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
